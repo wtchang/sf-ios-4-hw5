d1b1740d0f22ff3cde5715ba5f15bb2419955e14
Kathryn Rotondo <krotondo@gmail.com>
2016-01-20 17:46:29 -0800

    added project starter files

---
+++ src/iOS4-HW5-Delegation/AddItemViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddItemViewController.swift
+//  iOS4-HW5-Delegation
+//
+//  Created by Kathryn Rotondo on 1/20/16.
+//  Copyright © 2016 Kathryn Rotondo. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    @IBOutlet weak var itemTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func saveButtonPressed(sender: AnyObject) {
+    }
+}
---
+++ src/iOS4-HW5-Delegation/ItemsTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ItemsTableViewController.swift
+//  iOS4-HW5-Delegation
+//
+//  Created by Kathryn Rotondo on 1/20/16.
+//  Copyright © 2016 Kathryn Rotondo. All rights reserved.
+//
+
+import UIKit
+
+class ItemsTableViewController: UITableViewController {
+
+    var items:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("itemCellIdentifier", forIndexPath: indexPath)
+    
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
